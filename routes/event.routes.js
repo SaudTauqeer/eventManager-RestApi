85fc4a784a0525f4b3f010a860382ea65464ed4f
Saud Tauqeer <saudtauqeer381@gmail.com>
2018-12-01 23:52:57 +0500

    event.routes.js --> invalidFormStatement --> more conditions.

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -7,18 +7,19 @@ router.post("/api/event", (req, res)=>{
 
     // statment that checks if form data is okay.
     const invalidFormStatement = 
-    req.body.event || req.body.sendingHour
+    (req.body.event || req.body.sendingHour
     || req.body.sendingMinutes ||
     req.body.date || req.body.text ||
     req.body.subject || req.body.to ||
-    req.body.from === null;
+    req.body.from  === null || "" || " " || undefined);
 
 
     //if not a authorized user.
     if (!req.user) {res.status(401).send('Not authorized to view this page.')};
+
     if (req.user) {
     // Form data empty or not a user return 400 code.
-        if (!req.user || invalidFormStatement) {
+        if (invalidFormStatement) {
             res.status(400).send('Bad Request');
         }
         // if form is valid
