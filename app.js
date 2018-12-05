fb8fae47bd1a359c349b7628ea9937c90784a892
Saud Tauqeer <saudtauqeer381@gmail.com>
2018-12-06 02:47:53 +0500

    cors config --> app.js

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ var cors = require('cors');
 require('dotenv').config();
 const express = require("express");
 const app = express();
+app.use(cors());
 const port = process.env.PORT;
 
-app.use(cors({origin: 'https://eventmanager-web.herokuapp.com'}));
+app.use(function (req, res, next) {
 
+// Add headers
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://eventmanager-web.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // server config imports and routes
 const passportSetup = require('./config/passportSetup');
